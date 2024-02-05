694c61781b2ac4fa08367c1739add3f2d37c7d36
Alexander Litzow <57077920+thealexclub@users.noreply.github.com>
2024-02-04 22:25:28 -0500

    Style updates to match rapidplatform.com (#42)
    * Footer has been SWIZZLED! it now matches rapidplatform.com
    
    * adding slanty svg to bottom of hero banner
    
    * Delete static/img/rpd_white.svg:Zone.Identifier
    
    According to some light [googling])https://github.com/microsoft/WSL/issues/4609) it looks like this isn't a real file. When trying to pull the branch on windows this invalid extension prevents it so I'm removing it under the assumption it's trash
    
    ---------
    
    Co-authored-by: MattFranklin-Rapid <66400144+MattFranklin-Rapid@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,10 @@ export default function Home(): JSX.Element {
     <Layout title={`${siteConfig.title}`} description="Rapid Platform Documentation">
       <HeroBanner title={siteConfig.title} subTitle={siteConfig.tagline} />
 
+      <svg id="divider" viewBox="0 0 100 100" preserveAspectRatio="none">
+        <path d="m 0,100 h 100 l 0,-90z" id="slant" />
+      </svg>
+
       <main>
         <FeatureList />
       </main>
