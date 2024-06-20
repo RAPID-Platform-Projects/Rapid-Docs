796ebdc52ecce840e6a0e264836736f7986c54f1
MattFranklin-Rapid <66400144+MattFranklin-Rapid@users.noreply.github.com>
2024-06-20 14:40:22 +1000

    FEAT: Updated images for front page, added link components, cleaned up styles (#210)

--- src/components/page-components/home/feature-card/index.tsx
+++ src/components/page-components/home/feature-card/index.tsx
@@ -1,21 +1,25 @@
+import Link from "@docusaurus/Link";
 import Heading from "@theme/Heading";
 
 interface IProps {
   title: string;
   Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: string;
+  link: string;
 }
 
 function FeatureCard(props: IProps) {
   return (
-    <div className="col">
+    <div className="col featureList">
+      <Link href={props.link} className="link-no-hover">
       <div className="text--center">
         <props.Svg role="img" style={{ height: 200, width: 200 }} />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{props.title}</Heading>
-        <p>{props.description}</p>
+        <p className="featureListDescription">{props.description}</p>
       </div>
+      </Link>
     </div>
   );
 }
